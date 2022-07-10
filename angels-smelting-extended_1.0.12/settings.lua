bcfb0c05fef6e3c995a7a8e72d44fde60de0368a
Pezzawinkle <54166225+Pezzawinkle@users.noreply.github.com>
2022-07-10 12:50:49 +1000

    PCP updates

---
+++ angels-smelting-extended_1.0.12/settings.lua
@@ -0,0 +1,17 @@
+data:extend(
+{
+  {
+    type = "bool-setting",
+    name = "ASE-angels-coil-icons",
+    setting_type = "startup",
+    default_value = false,
+    order = "a",
+  },
+  {
+    type = "bool-setting",
+    name = "ASE-angels-molten-recipes",
+    setting_type = "startup",
+    default_value = false,
+    order = "b",
+  }
+})
